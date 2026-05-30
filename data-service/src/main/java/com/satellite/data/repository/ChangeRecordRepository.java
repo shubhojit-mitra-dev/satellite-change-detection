417f74120b49333223bfbbaaa2fcacdf361e9db7
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-05-31 02:00:38 +0530

    feat: add findByFieldId method to ChangeRecordRepository

--- data-service/src/main/java/com/satellite/data/repository/ChangeRecordRepository.java
+++ data-service/src/main/java/com/satellite/data/repository/ChangeRecordRepository.java
@@ -4,8 +4,10 @@ import com.satellite.data.entity.ChangeRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ChangeRecordRepository extends JpaRepository<ChangeRecord, UUID> {
+    List<ChangeRecord> findByFieldId(UUID fieldId);
 }
