e0d1332c4ac2b1fae50e51ac2c1c6ed87e1b2af4
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-05-30 20:29:29 +0530

    change-detection - initial commit

---
+++ change-detection-service/src/main/java/com/satellite/changedetection/ChangeDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.satellite.changedetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeDetectionApplication.class, args);
+	}
+
+}
