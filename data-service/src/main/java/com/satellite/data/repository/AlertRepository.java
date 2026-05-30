6cfe15532b05c7bbeb4a680de7e3e64cb16324a3
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-05-31 02:00:24 +0530

    feat: add findByFieldId method to AlertRepository

--- data-service/src/main/java/com/satellite/data/repository/AlertRepository.java
+++ data-service/src/main/java/com/satellite/data/repository/AlertRepository.java
@@ -4,8 +4,10 @@ import com.satellite.data.entity.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AlertRepository extends JpaRepository<Alert, UUID> {
+    List<Alert> findByFieldId(UUID fieldId);
 }
