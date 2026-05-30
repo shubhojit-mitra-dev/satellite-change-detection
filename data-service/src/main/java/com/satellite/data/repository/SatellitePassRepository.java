c9ce2a10ef4ffa6a359701cf5396573cd64125d8
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-05-31 01:46:44 +0530

    feat: add SatellitePassRepository for data access operations

---
+++ data-service/src/main/java/com/satellite/data/repository/SatellitePassRepository.java
@@ -0,0 +1,11 @@
+package com.satellite.data.repository;
+
+import com.satellite.data.entity.SatellitePass;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SatellitePassRepository extends JpaRepository<SatellitePass, UUID> {
+}
