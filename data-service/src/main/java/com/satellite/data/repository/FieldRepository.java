f80146fb923f912d53c17e8e489b61eda57173e9
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-05-31 01:46:40 +0530

    feat: add FieldRepository for data access operations

---
+++ data-service/src/main/java/com/satellite/data/repository/FieldRepository.java
@@ -0,0 +1,11 @@
+package com.satellite.data.repository;
+
+import com.satellite.data.entity.Field;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface FieldRepository extends JpaRepository<Field, UUID> {
+}
