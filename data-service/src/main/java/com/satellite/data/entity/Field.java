63b150cc875e055b1426c4701fa2a386d7c18522
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-05-31 01:43:24 +0530

    feat: add Field JPA entity with JTS geometry support for satellite boundary tracking

---
+++ data-service/src/main/java/com/satellite/data/entity/Field.java
@@ -0,0 +1,38 @@
+package com.satellite.data.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.locationtech.jts.geom.Geometry;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "fields")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Field {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "boundary", columnDefinition = "geometry(Polygon,4326)")
+    private Geometry boundary;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
