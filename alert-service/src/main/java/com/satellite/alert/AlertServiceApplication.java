f0685aff77b84ad4b609ab1dd845082f620e0e07
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-05-31 15:44:19 +0530

    feat: add KafkaTopicsConfig and enable properties in AlertServiceApplication

--- alert-service/src/main/java/com/satellite/alert/AlertServiceApplication.java
+++ alert-service/src/main/java/com/satellite/alert/AlertServiceApplication.java
@@ -1,9 +1,12 @@
 package com.satellite.alert;
 
+import com.satellite.alert.config.KafkaTopicsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KafkaTopicsConfig.class)
 public class AlertServiceApplication {
 
 	public static void main(String[] args) {
