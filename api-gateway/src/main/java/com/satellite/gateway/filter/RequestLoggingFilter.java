e802f14829dead3ab7dc5e282ba139c54ce43df6
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-05-31 17:38:36 +0530

    feat: add global request logging filter for API gateway requests

---
+++ api-gateway/src/main/java/com/satellite/gateway/filter/RequestLoggingFilter.java
@@ -0,0 +1,29 @@
+package com.satellite.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class RequestLoggingFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String method = exchange.getRequest().getMethod().name();
+        String path = exchange.getRequest().getURI().getPath();
+        
+        log.info("Incoming request: {} {}", method, path);
+        
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
